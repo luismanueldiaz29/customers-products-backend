65ee159ac720f2a21866c8836501bb2aa09d6c5d
Luis Diaz <luis.diaz@fsteam.co>
2024-03-06 23:15:14 -0500

    first commit

---
+++ src/main/java/com/luis/technical/test/api/customers/products/CustomersProductsApplication.java
@@ -0,0 +1,13 @@
+package com.luis.technical.test.api.customers.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/technical/test/api/customers/products/CustomersProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.technical.test.api.customers.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
